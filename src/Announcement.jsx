edc9a4d548055b49a78fff67aab84bdc1fc4ec42
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-03-09 16:41:39 +0000

    Update importing of react method

--- src/Announcement.jsx
+++ src/Announcement.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Announcement extends Component{
+export default class Announcement extends React.Component{
   render() {
     return (
       <div className='visible'>
--- src/Tile.jsx
+++ src/Tile.jsx
@@ -1,7 +1,7 @@
-import React { Component } from 'react';
+import React from 'react';
 import './Tile.css'
 
-export default class Tile extends Component {
+export default class Tile extends React.Component {
   tileClick(props) {
     props.updateBoard(props.loc, props.turn);
   }
