646284efab1de5924807413de6cb769aa9caacf6
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-03-09 15:04:22 +0000

    Start building scaffold of what main page will have: Announcement component for when game ends.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-
+      <div className ="container">
+        <div className="menu">
+          <h1>Tic-Tac-Toe</h1>
+          <Announcement />
+        </div>
+      </div>
     );
   }
 }
